f948709a8e2c89716dde6382837a63364e06cba8
mbsomeday <137542994@qq.com>
2023-12-20 20:26:56 +0000

    训练数据加载

--- cv_models/VGG/ww_data.py
+++ cv_models/VGG/ww_data.py
@@ -3,24 +3,42 @@ import torch.nn as nn
 from torch import optim
 from torch.utils.data import DataLoader, Dataset
 from torchvision import transforms, utils, datasets
+from torchvision import transforms as T
 
 
 from ww_model import vgg16_bn
 
+mean = (0.4914, 0.4822, 0.4465)
+std = (0.2471, 0.2435, 0.2616)
 batch_size = 64
+
+def test_dataloader():
+    transform = T.Compose(
+        [
+            T.ToTensor(),
+            T.Normalize(mean, std),
+        ]
+    )
+    dataset = datasets.CIFAR10(root='./data', train=False, transform=transform)
+    dataloader = DataLoader(
+        dataset,
+        batch_size=batch_size,
+        drop_last=True,
+        pin_memory=True,
+    )
+    return dataset, dataloader
+
+
 # num_classes = 10
 if torch.cuda.is_available():
     use_gpu = True
 else:
     use_gpu = False
 
-# 下载训练集 CIFAR-10训练集
-# train_dataset = datasets.CIFAR10('./data', train=True, transform=transforms.ToTensor(), download=True)
-# train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
-test_dataset = datasets.CIFAR10('./data', train=False, transform=transforms.ToTensor(), download=True)
-test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
 
-model = vgg16_bn()
+test_dataset, test_loader = test_dataloader()
+
+model = vgg16_bn(pretrained=True)
 
 # 测试开始
 model.eval()
